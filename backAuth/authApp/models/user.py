c294e99cf79eed5189f683a9121838f724921b66
DeiverV <deiberveranodev@gmail.com>
2021-11-11 22:00:08 -0500

    se añade el back del microservicio de autenticacion

---
+++ backAuth/authApp/models/user.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,BaseUserManager
+from django.db.models.fields import EmailField
+from django.contrib.auth.hashers import make_password
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, password=None):
+        """
+        Creates and saves a user with the given username and password.
+        """
+        if not username:
+            raise ValueError('Users must have an username')
+        user = self.model(username=username)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+def create_superuser(self, username, password):
+        """
+        Creates and saves a superuser with the given username and password.
+        """
+        user = self.create_user(
+            username=username,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.BigAutoField(primary_key=True)
+    rol = models.CharField('Rol',max_length=15,default='consultor')
+    username = models.CharField('Username',max_length=15,unique=True)
+    password = models.CharField('Password',max_length=256)
+    name = models.CharField('Name', max_length=30)
+    email = models,EmailField('Email',max_length=150)
+
+    #para encriptar contraseñas
+    def save(self, **kwargs):
+        some_salt='mMuj0DrIK6vgtdIYepKIxN'
+        self.password = make_password(self.password, some_salt)
+        super().save(**kwargs)
+    
+    USERNAME_FIELD ='username'
+    objects = UserManager()
---
+++ backAuth/auth_ms/Scripts/django-admin.py
@@ -0,0 +1,21 @@
+#!c:\users\deive\onedrive\escritorio\mintic\ciclo4\configuracionlogin\auth_ms\scripts\python.exe
+# When the django-admin.py deprecation ends, remove this script.
+import warnings
+
+from django.core import management
+
+try:
+    from django.utils.deprecation import RemovedInDjango40Warning
+except ImportError:
+    raise ImportError(
+        'django-admin.py was deprecated in Django 3.1 and removed in Django '
+        '4.0. Please manually remove this script from your virtual environment '
+        'and use django-admin instead.'
+    )
+
+if __name__ == "__main__":
+    warnings.warn(
+        'django-admin.py is deprecated in favor of django-admin.',
+        RemovedInDjango40Warning,
+    )
+    management.execute_from_command_line()
